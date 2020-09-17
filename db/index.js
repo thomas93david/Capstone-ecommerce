1c2e89b52c24f1c2c2115f7671e34db73798d3b5
causey904.bc <your_email@real_email.edu>
2020-09-17 18:55:52 -0400

    adding database functions to movies carts join table

--- db/index.js
+++ db/index.js
@@ -2,6 +2,4 @@ module.exports = {
     ...require('./customers.js'),
     ...require('./movies.js'),
     ...require('./cart.js'),
-    ...require('./wishlist.js'),
-    ...require('./customer_cart.js')
 };
