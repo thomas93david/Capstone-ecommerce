0f6e60a37019fcde67913888976c67ba5390f2fd
Chelsea <cw.19courtney@gmail.com>
2020-10-03 23:48:33 -0400

    wtf git

--- src/components/App.js
+++ src/components/App.js
@@ -43,7 +43,7 @@ function App() {
               )}
             /> : <>
               <Route
-                path="/register"
+                 path="/register"
                 exact
                 render={() => (
                   <RegisterPage customer={customer} setCustomer={setCustomer} />
